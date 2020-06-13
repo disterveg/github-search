2d817abc4dc4557dbe18924e517167d7bb2ac329
radrigo <cherkashin.da@gazprom-neft.ru>
2020-06-13 19:25:08 +0300

    create component navbar and pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from '../../pages/Home';
 
 function App() {
   return (
     <div className="container pt-4">
-      <h1>Test</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Navbar = () => (
+  <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+    <div className="navbar-brand">
+      Github Search
+    </div>
+    <ul>
+      <li className="nav-item">
+      </li>
+    </ul>
+  </nav>
+);
