8442adf2a09003269693efb6df973f8880d18ac7
radrigo <cherkashin.da@gazprom-neft.ru>
2020-06-14 17:49:51 +0300

    create component alert

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,24 @@ import {Navbar} from '../../components/Navbar/Navbar';
 import {Home} from '../../pages/Home';
 import {About} from '../../pages/About';
 import {Profile} from '../../pages/Profile';
+import {Alert} from '../Alert/Alert';
+import {AlertState} from '../../context/alert/alertState';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile/:name" component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert alert={{text: 'Test alert'}} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/profile/:name" component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
