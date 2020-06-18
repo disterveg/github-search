03d4b67f1f0d067b12c390ee8d8f69e101bc360a
radrigo <cherkashin.da@gazprom-neft.ru>
2020-06-18 22:56:26 +0300

    implementation home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Search} from '../components/Search/Search';
 import {Card} from '../components/Card/Card';
+import {GithubContext} from '../context/github/githubContext';
 
 export const Home = () => {
-  const cards = new Array(15)
-      .fill('')
-      .map((_, i) => i);
+  const {loading, users} = useContext(GithubContext);
   return (
     <React.Fragment>
       <Search />
       <div className="row">
-        {cards.map(card => {
-          return (
-            <div className="col-sm-4 mb-4" key={card}>
-              <Card />
+        {
+          loading ?
+          <p className="text-center">Загрузка..</p> :
+          users.map(user => (
+            <div className="col-sm-4 mb-4" key={user.id}>
+              <Card user={user} />
             </div>
-          );
-        })}
+          ))
+        }
 
       </div>
     </React.Fragment>
